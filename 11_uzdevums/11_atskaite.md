409ac90bfd7441e70e805e3eab280078f4dbc63e
GROUPAD1\pikseri <rinalds.pikse@intrum.com>
2023-02-06 18:27:38 +0200

    11_uzd initial sucess without bayesian

---
+++ 11_uzdevums/11_atskaite.md
@@ -0,0 +1,73 @@
+### 11 bayesian regresison task 
+
+Sākumā mēģināju uztaisīt parastu modeli, kas peredzēs māju cenu no dotajiem atribūtiem.
+
+Man kkas nesanāca - daudz cīnijos ar datuseta importēšanu - nebiju lietojis sklearn:
+~~~
+housing = datasets.fetch_california_housing()
+print(housing.feature_names)
+
+x = housing.data
+y = housing.target
+
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
+x_train = torch.tensor(x_train)
+x_test = torch.tensor(x_test)
+y_train = torch.tensor(y_train)
+y_test = torch.tensor(y_test)
+~~~
+
+Kad dati sadalīti šādi - īsti nezinu kā konstruēt epoch ciklus
+
+Es implementēju dataset un dataloader lai sanāktu izveidot vismaz strādājosu modeli:
+
+~~~
+class Model(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.layers = torch.nn.Sequential(
+            torch.nn.Linear(in_features=8, out_features=8),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(in_features=8, out_features=16),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(in_features=16, out_features=8),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(in_features=8, out_features=4),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(in_features=4, out_features=1),
+            torch.nn.Softmax()
+        )
+
+        self.nn_layers = torch.nn.ModuleList()
+
+    def forward(self, x):
+        out = x
+        for layer in self.layers:
+            out = layer.forward(out)
+        return out
+        # y_prim = self.layers.forward(x)
+        # return y_prim
+
+    def backward(self):
+        for layer in reversed(self.layers):
+            layer.backward()
+~~~
++ MSELoss un Adam optimizer:
+~~~
+optimizer = torch.optim.Adam(
+    model.parameters(),
+    lr=LEARNING_RATE
+)
+criteria = torch.nn.MSELoss()
+~~~
+Bet rezultāts izskatās nepareizs:
+![11_failed_linear_model.PNG](..%2Fmedia%2F11_failed_linear_model.PNG)
+
+Tad nomainīju modeli uz 
+~~~
+model = torch.nn.Linear(8, 1)
+~~~
+Rezultāts izskatās ticamāks: 
+![11_torch_linear.PNG](..%2Fmedia%2F11_torch_linear.PNG)
--- 11_uzdevums/11_bayesian.py
+++ 11_uzdevums/11_bayesian.py
@@ -8,8 +8,9 @@ import scipy.stats
 import matplotlib.pyplot as plt
 
 
-BATCH_SIZE = 18
+BATCH_SIZE = 128
 LEARNING_RATE = 0.001
+TRAIN_TEST_SPLIT = 0.7
 
 housing = datasets.fetch_california_housing()
 print(housing.feature_names)
@@ -17,12 +18,54 @@ print(housing.feature_names)
 x = housing.data
 y = housing.target
 
+#x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
+#x = torch.from_numpy(x.astype(np.float32))
+#y = torch.from_numpy(y.astype(np.float32))
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
-x_train = torch.tensor(x_train)
-x_test = torch.tensor(x_test)
-y_train = torch.tensor(y_train)
-y_test = torch.tensor(y_test)
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self):
+        super().__init__()
+
+        X = housing.data
+        self.X = torch.from_numpy(x.astype(np.float32))
+        Y = housing.target
+        Y = torch.LongTensor(Y.astype(np.float32))
+        self.Y = Y.unsqueeze(dim=-1)
+
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        x = self.X[idx]
+
+        y = self.Y[idx]
+        y= y.float()
+
+        return x, y
+
+
+dataset_housing = Dataset()
+train_test_split = int(len(dataset_housing) * TRAIN_TEST_SPLIT)
+dataset_train, dataset_test = torch.utils.data.random_split(
+    dataset_housing,
+    [train_test_split, len(dataset_housing) - train_test_split],
+    generator=torch.Generator().manual_seed(0)
+)
+
+dataloader_train = torch.utils.data.DataLoader(
+    dataset=dataset_train,
+    batch_size=BATCH_SIZE,
+    shuffle=True
+)
+
+dataloader_test = torch.utils.data.DataLoader(
+    dataset=dataset_test,
+    batch_size=BATCH_SIZE,
+    shuffle=False
+)
+
 mu = 10
 sigma = 2.
 
@@ -31,13 +74,11 @@ class Model(torch.nn.Module):
         super().__init__()
 
         self.layers = torch.nn.Sequential(
-            torch.nn.Linear(in_features=8, out_features=12),
+            torch.nn.Linear(in_features=8, out_features=8),
             torch.nn.Sigmoid(),
-            torch.nn.Linear(in_features=12, out_features=16),
+            torch.nn.Linear(in_features=8, out_features=16),
             torch.nn.Sigmoid(),
-            torch.nn.Linear(in_features=16, out_features=12),
-            torch.nn.Sigmoid(),
-            torch.nn.Linear(in_features=12, out_features=8),
+            torch.nn.Linear(in_features=16, out_features=8),
             torch.nn.Sigmoid(),
             torch.nn.Linear(in_features=8, out_features=4),
             torch.nn.Sigmoid(),
@@ -48,12 +89,12 @@ class Model(torch.nn.Module):
         self.nn_layers = torch.nn.ModuleList()
 
     def forward(self, x):
-        # out = x
-        # for layer in self.layers:
-        #     out = layer.forward(out)
-        # return out
-        y_prim = self.layers.forward(x)
-        return y_prim
+        out = x
+        for layer in self.layers:
+            out = layer.forward(out)
+        return out
+        # y_prim = self.layers.forward(x)
+        # return y_prim
 
     def backward(self):
         for layer in reversed(self.layers):
@@ -61,7 +102,7 @@ class Model(torch.nn.Module):
 
 
 
-model = Model()
+model = torch.nn.Linear(8, 1)#Model()
 optimizer = torch.optim.Adam(
     model.parameters(),
     lr=LEARNING_RATE
@@ -72,22 +113,34 @@ loss_plot_train = []
 loss_plot_test = []
 for epoch in range(1, 1000):
 
-    losses = []
-    for x in (x_train):
-        #forwards
-        y_prim = model.forward(x)
-        loss = criteria(y_prim, y)
+    for dataloader in [dataloader_train, dataloader_test]:
+        losses = []
+        for x, y in dataloader:
 
-        #backwards
-        loss.backward()
+            y_prim = model.forward(x)
+            loss = criteria.forward(y_prim, y)
 
-        #update
-        optimizer.step()
-        optimizer.zero_grad()
+            losses.append(loss.item())
 
-    if (epoch+1) % 10 ==0:
-        print(f'epoch: {epoch+1}, loss = {loss.item():.4f}')
+            if dataloader == dataloader_train:
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
 
-#plot
-#predicted = model(x).detach().numpy()
-#plt.plot(x)
+        if dataloader == dataloader_train:
+            loss_plot_train.append(np.mean(losses))
+        else:
+            loss_plot_test.append(np.mean(losses))
+
+    print(f'epoch: {epoch} loss_train: {loss_plot_train[-1]} loss_test: {loss_plot_test[-1]}')
+
+    if epoch % 10 == 0:
+        fig, ax1 = plt.subplots()
+        ax1.plot(loss_plot_train, 'r-', label='train')
+        ax2 = ax1.twinx()
+        ax2.plot(loss_plot_test, 'c-', label='test')
+        ax1.legend()
+        ax2.legend(loc='upper left')
+        ax1.set_xlabel("Epoch")
+        ax1.set_ylabel("Loss")
+        plt.show()
